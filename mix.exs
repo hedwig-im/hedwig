8567d8f1cfcf613fb252bb05fdd179449b1c5240
Sonny Scroggin <scrogson@gmail.com>
2014-04-01 21:32:41 -0500

    Living on the edge!

--- mix.exs
+++ mix.exs
@@ -4,20 +4,14 @@ defmodule Hedwig.Mixfile do
   def project do
     [ app: :hedwig,
       version: "0.0.1",
-      elixir: "~> 0.12.5",
+      elixir: "~> 0.13.0-dev",
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     []
   end
