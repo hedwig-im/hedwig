3e90ea092559c073ed45495b8a8dfae127b34efc
Sonny Scroggin <scrogson@gmail.com>
2014-05-28 02:28:09 -0500

    Update Supervisor

--- lib/hedwig/supervisor.ex
+++ lib/hedwig/supervisor.ex
@@ -1,12 +1,14 @@
 defmodule Hedwig.Supervisor do
-  use Supervisor.Behaviour
+  use Supervisor
 
   def start_link(opts) do
-    :supervisor.start_link(__MODULE__, opts)
+    Supervisor.start_link(__MODULE__, opts)
   end
 
   def init(opts) do
-    children = [ worker(Hedwig.Client, []) ]
+    children = [
+      worker(Hedwig.Client, [])
+    ]
     supervise children, strategy: :one_for_one
   end
 end
