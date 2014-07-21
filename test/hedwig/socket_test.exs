40f526b9173335d1fcb14cc0c2b0af79f89db23c
Sonny Scroggin <scrogson@gmail.com>
2014-07-20 20:09:20 -0500

    Remove `Hedwig.Socket`

--- test/hedwig/socket_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Hedwig.SocketTest do
-  use ExUnit.Case, async: true
-
-  test "it returns a socket" do
-    {:ok, socket} = Hedwig.Socket.connect "localhost"
-    assert is_port(socket)
-  end
-end
