10585a3d77aa2aff81c8af0c7378cd24c31ecfbc
Sonny Scroggin <scrogson@gmail.com>
2014-07-26 23:38:36 -0500

    Update Supervisor

--- lib/hedwig.ex
+++ lib/hedwig.ex
@@ -1,7 +1,15 @@
 defmodule Hedwig do
   use Application
 
-  def start(_type, opts) do
-    Hedwig.Supervisor.start_link(opts)
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    clients = Application.get_env(:hedwig, :clients, [])
+    children = for client <- clients, into: [] do
+      worker(Hedwig.Client, [client.jid])
+    end
+
+    opts = [strategy: :simple_one_for_one, name: Hedwig.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
