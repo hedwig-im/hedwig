1c99ec2c08c69ba36021a9ecccb0d3e31f9f701a
Sonny Scroggin <scrogson@gmail.com>
2014-07-30 02:07:39 -0500

    Refactored and working client

--- lib/hedwig/client.ex
+++ lib/hedwig/client.ex
@@ -4,99 +4,62 @@ defmodule Hedwig.Client do
   """
 
   use GenServer
+
+  require Logger
+
+  alias Hedwig.JID
+  alias Hedwig.Conn
+  alias Hedwig.Client
   alias Hedwig.Stanza
 
-  defmodule State do
-    defstruct event_handlers: [],
-              server: "",
-              port: 5222,
-              socket: nil,
-              connected?: false,
-              authenticated?: false,
-              jid: "",
-              pass: "",
-              nick: "",
-              rooms: [],
-              parser: nil
-  end
+  @type t :: %__MODULE__{}
+  @derive [Access, Enumerable]
+  defstruct jid: "",
+            nickname: "",
+            resource: "",
+            conn: %Conn{},
+            config: [],
+            rooms: [],
+            event_handlers: []
 
-  # Public API
+  @spec start_link(client :: list) :: {:ok, pid}
+  def start_link(client) do
+    GenServer.start_link(__MODULE__, to_struct(client), [])
+  end
 
   @doc """
-  Connect to the XMPP server.
+  Returns the client config for the given JID.
   """
-  def connect(pid) do
-    GenServer.call pid, :connect
-  end
-
-  def start_stream(pid) do
-    GenServer.call pid, :start_stream
-  end
-
-  def start_tls(pid) do
-    GenServer.call pid, :start_tls
-  end
-
-  def send(pid, data) do
-    GenServer.call pid, {:send, data}
-  end
-
-  # GenServer API
-
-  def start_link(options \\ []) do
-    GenServer.start_link(__MODULE__, options, [])
-  end
-
-  @spec init(list(any) | []) :: {:ok, State.t}
-  def init(options \\ []) do
-    server = Keyword.get(options, :server, "localhost")
-    port   = Keyword.get(options, :port, 5222)
-    {:ok, parser} = :exml_stream.new_parser
-
-    {:ok, %State{server: server, port: port, parser: parser}}
-  end
-
-  def handle_call(:connect, _from, %State{server: server, port: port} = state) do
-    if state.socket != nil, do: :gen_tcp.close(state.socket)
-
-    case Hedwig.Socket.connect(server, port) do
-      {:ok, socket} ->
-        {:reply, :ok, %State{state| socket: socket, connected?: true} }
-      error ->
-        {:reply, {:error, error}, state}
+  def client_for(jid) do
+    client = Enum.find Application.get_env(:hedwig, :clients), fn client ->
+      client.jid == jid
     end
   end
 
-  def handle_call(:start_stream, _from, state) do
-    stanza = Stanza.start_stream state.server
-    Hedwig.Socket.send(state.socket, stanza)
-    {:reply, :ok, state}
+  def normalize_config(client) do
+    %JID{server: server} = JID.parse(client.jid)
+
+    config = if client[:config], do: client[:config], else: []
+    |> Keyword.put_new(:server, server)
+    |> Keyword.put_new(:port, 5222)
+    |> Keyword.put_new(:require_tls?, false)
+    |> Keyword.put_new(:use_compression?, false)
+    |> Keyword.put_new(:use_stream_management?, false)
+    |> Keyword.put_new(:transport, :tcp)
+
+    Map.put(client, :config, config)
   end
 
-  def handle_call(:start_tls, _from, state) do
-    Hedwig.Socket.send state.socket, Stanza.start_tls
-    {:reply, :ok, state}
+  @doc """
+  Converts a map to a Client struct.
+  """
+  def to_struct(client) do
+    client = normalize_config(client)
+    struct(Client, Map.to_list(client))
   end
 
-  def handle_call({:send, data}, _from, state) do
-    Hedwig.Socket.send state.socket, data
-    {:reply, :ok, state}
-  end
-
-  def handle_info({:tcp, socket, data}, state) do
-    {:ok, parser, packet} = :exml_stream.parse(state.parser, data)
-    IO.puts "====================== PACKET =============================="
-    IO.inspect packet
-    IO.puts "==================== END PACKET ============================"
-    {:noreply, %{state| parser: parser}}
-  end
-
-  def handle_info({:tcp_closed, _socket}, state) do
-    IO.puts "Socket closed"
-    {:noreply, %{state| parser: nil, socket: nil, connected?: false}}
-  end
-
-  def handle_info({_msg, state}) do
-    {:noreply, state}
+  def init(client) do
+    conn = spawn_link(Conn, :start, [client])
+    {:ok, %Client{conn: conn}}
   end
 end
