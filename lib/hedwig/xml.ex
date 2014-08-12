26dd8232c7bfa59cee12204ba2e90dd506062648
Sonny Scroggin <scrogson@gmail.com>
2014-08-12 15:38:29 -0500

    Update Hedwig.XML

--- lib/hedwig/xml.ex
+++ lib/hedwig/xml.ex
@@ -1,12 +1,13 @@
 defmodule Hedwig.XML do
   @moduledoc """
-  Provides records for exml library.
+  Provides functions and records for exml library.
   """
   defmacro __using__(_opts) do
     quote do
       use Hedwig.XMLNS
       require Record
       import unquote __MODULE__
+      alias unquote __MODULE__
       alias Hedwig.Stanza
 
       Record.defrecordp :xmlel, name: "", attrs: [], children: []
@@ -15,4 +16,17 @@ defmodule Hedwig.XML do
       Record.defrecordp :xmlstreamend, name: ""
     end
   end
+
+  @doc """
+  Returns the given attribute value or default.
+  """
+  def attr(element, name, default \\ nil) do
+    :exml_query.attr(element, name, default)
+  end
+
+  def subelement(element, name, default \\ nil) do
+    :exml_query.subelement(element, name, default)
+  end
+
+  def cdata(element), do: :exml_query.cdata(element)
 end
