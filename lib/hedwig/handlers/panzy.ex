2882f55011d27332ab84bb8e34a975587b6ec509
Sonny Scroggin <scrogson@gmail.com>
2014-08-26 17:44:36 -0500

    Panzy handler

---
+++ lib/hedwig/handlers/panzy.ex
@@ -0,0 +1,27 @@
+defmodule Hedwig.Handlers.Panzy do
+  @moduledoc """
+  Says 'Panzy!' whenever someone says 'hard'.
+  """
+
+  use Hedwig.Handler
+
+  def init(opts), do: {:ok, opts}
+
+  def handle_event(%Message{delayed?: false} = msg, opts) do
+    cond do
+      hear ~r/tired|too hard|to hard|upset|bored/i, msg ->
+        panzy!(msg)
+      true ->
+        :ok
+    end
+    {:ok, opts}
+  end
+
+  def handle_event(_, opts), do: {:ok, opts}
+
+  defp panzy!(msg) do
+    body = "Panzy!"
+    html = "<p><strong>#{body}</strong></p>"
+    reply(msg, [Stanza.body(body), Stanza.xhtml_im(html)])
+  end
+end
