b09f2457236358b07cac5d529d9f8a04762d6c09
Sonny Scroggin <scrogson@gmail.com>
2014-11-23 12:33:33 -0600

    Update conn_test

--- test/hedwig/conn_test.exs
+++ test/hedwig/conn_test.exs
@@ -4,11 +4,19 @@ defmodule Hedwig.ConnTest do
 
   alias Hedwig.Client
 
-  # TODO: Figure out how to really test this better.
-  test "it connects" do
-    client = Client.client_for(System.get_env("XMPP_JID"))
+  setup client do
+    {:ok, %{
+      jid:      System.get_env("TEST_XMPP_JID"),
+      password: System.get_env("TEST_XMPP_PASS"),
+      nickname: System.get_env("TEST_XMPP_NICK"),
+      resource: System.get_env("TEST_XMPP_RESOURCE") || "hedwig",
+     }}
+  end
+
+  test "it connects", client do
+    {:ok, pid} = Client.start_link(client)
     assert capture_io(:user, fn ->
-      :timer.sleep(500)
+      :timer.sleep(300)
     end) =~ ~r/#{client.jid} successfully connected/i
   end
 end
