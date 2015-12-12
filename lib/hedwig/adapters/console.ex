90697dc05533e45073dbaddfba29fca38f931088
Sonny Scroggin <scrogson@gmail.com>
2015-12-11 21:48:41 -0600

    Improve the console adapter

--- lib/hedwig/adapters/console.ex
+++ lib/hedwig/adapters/console.ex
@@ -1,30 +1,47 @@
 defmodule Hedwig.Adapters.Console do
   use Hedwig.Adapter
 
+  ## Adapter API
+
   def send(pid, msg) do
-    Kernel.send(pid, {:send, msg})
+    GenServer.cast(pid, {:reply, msg})
   end
 
   def reply(pid, msg) do
-    Kernel.send(pid, msg)
+    GenServer.cast(pid, {:reply, msg})
   end
 
-  def handle_call({:reply, msg}, _from, state) do
-    Kernel.send(state.conn, msg)
-    {:reply, :ok, state}
+  def emote(pid, msg) do
+    GenServer.cast(pid, {:emote, msg})
+  end
+
+  ## Callbacks
+
+  def handle_cast({:send, msg}, %{conn: conn} = state) do
+    Kernel.send(conn, {:reply, msg})
+    {:noreply, state}
+  end
+
+  def handle_cast({:reply, msg}, %{conn: conn} = state) do
+    Kernel.send(conn, {:reply, msg})
+    {:noreply, state}
+  end
+
+  def handle_cast({:emote, %{text: text} = msg}, %{conn: conn} = state) do
+    Kernel.send(conn, {:reply, msg})
+    {:noreply, state}
   end
 
   def handle_info({:message, ""}, state) do
     {:noreply, state}
   end
 
-  def handle_info({:message, text}, %{robot: robot} = %{conn: conn} = state) do
+  def handle_info({:message, text}, %{robot: robot, conn: conn} = state) do
     {user, 0} = System.cmd("whoami", [])
 
-    ref = make_ref()
     msg = %Hedwig.Message{
       adapter: {__MODULE__, self},
-      ref: ref,
+      ref: make_ref(),
       text: text,
       type: "chat",
       user: String.strip(user)
@@ -32,61 +49,60 @@ defmodule Hedwig.Adapters.Console do
 
     Hedwig.Robot.handle_message(robot, msg)
 
-    msg =
-      receive do
-        {:reply, %Hedwig.Message{ref: ^ref}} = reply ->
-          Kernel.send(conn, reply)
-        {:reply, nil} = reply ->
-          Kernel.send(conn, reply)
-      after
-        5_000 ->
-          Kernel.send(conn, {:reply, nil})
-      end
-
     {:noreply, state}
   end
 
   defmodule Connection do
 
     def connect(opts) do
+      {user, 0} = System.cmd("whoami", [])
+
       IO.puts [IO.ANSI.clear, IO.ANSI.home]
-      Task.start_link(__MODULE__, :loop, [self, opts[:name]])
+      display_banner()
+      Task.start_link(__MODULE__, :loop, [self, String.strip(user), opts[:name]])
     end
 
-    def loop(owner, name) do
-      name
+    def loop(owner, user, name) do
+      user
       |> prompt
       |> IO.gets
       |> String.strip
-      |> call_adapter(owner)
+      |> call_adapter(owner, name)
 
-      loop(owner, name)
+      loop(owner, user, name)
     end
 
-    defp call_adapter(text, owner) do
+    defp call_adapter(text, owner, name) do
       send(owner, {:message, text})
-      await()
+      await(name)
     end
 
-    defp await do
+    defp await(name) do
       receive do
         {:reply, resp} ->
-          handle_result(resp)
+          handle_result(resp, name)
       after
-        5_000 ->
-          :ok
+        1_000 -> :ok
       end
     end
+
+    ## IO
 
     defp prompt(name) do
       import IO.ANSI
       [white, bright, name, "> ", normal, default_color]
     end
 
-    defp handle_result(nil), do: nil
-    defp handle_result(msg) do
+    defp handle_result(nil, _name), do: nil
+    defp handle_result(msg, name) do
       import IO.ANSI
-      IO.puts [yellow, msg.text, default_color]
+      IO.puts prompt(name) ++ [yellow, msg.text, default_color]
+    end
+
+    defp display_banner do
+      IO.puts "Hedwig Console - press Ctrl+C to exit.\r\n"
+      IO.puts "The console adapter is useful for quickly verifying how your\r"
+      IO.puts "bot will respond based on the current installed responders.\r\n"
     end
   end
 end
