c4bca46abf08f73e5d2b472caa662598c4cd0920
Sonny Scroggin <scrogson@gmail.com>
2015-12-14 08:05:35 -0600

    Change the registry back to the way it was

--- lib/hedwig/registry.ex
+++ lib/hedwig/registry.ex
@@ -4,15 +4,8 @@ defmodule Hedwig.Registry do
   @doc """
   Registers a `name` for the current process.
   """
-  def register_name(name) do
+  def register(name) do
     true = :gproc.reg({:n, :l, name})
-  end
-
-  @doc """
-  Registers a `property` for the current process.
-  """
-  def register_property(property) do
-    true = :gproc.reg({:p, :l, property})
   end
 
   @doc """
@@ -20,16 +13,7 @@ defmodule Hedwig.Registry do
 
   Returns `pid` in case a robot exists, `:undefined` otherwise.
   """
-  def whereis_name(name) do
+  def whereis(name) do
     :gproc.whereis_name({:n, :l, name})
-  end
-
-  @doc """
-  Looks up the robot pid for the given `property`.
-
-  Returns `pid` in case a robot exists, `:undefined` otherwise.
-  """
-  def whereis_property(property) do
-    :gproc.whereis_name({:p, :l, property})
   end
 end
