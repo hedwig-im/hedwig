11083b0a450f3f9f2e8f8a92bc1a8c69368756b7
Sonny Scroggin <scrogson@gmail.com>
2015-12-14 21:13:05 -0600

    Add after_connect in test robot

--- test/support/test_robot.ex
+++ test/support/test_robot.ex
@@ -2,4 +2,8 @@ Code.ensure_compiled(Hedwig.Adapters.Test)
 
 defmodule Hedwig.TestRobot do
   use Hedwig.Robot, otp_app: :hedwig, adapter: Hedwig.Adapters.Test
+
+  def after_connect(state) do
+    {:ok, state}
+  end
 end
