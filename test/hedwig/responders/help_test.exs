a42bdc45905243ae681f342cfaac2f4c3697eeba
Sonny Scroggin <scrogson@gmail.com>
2015-12-14 21:14:32 -0600

    Help test

---
+++ test/hedwig/responders/help_test.exs
@@ -0,0 +1,17 @@
+defmodule Hedwig.Responders.HelpTest do
+  use Hedwig.RobotCase
+
+  @tag start_robot: true, name: "alfred"
+  test "help - displays the usage for all installed responders", %{adapter: adapter, msg: msg} do
+    send adapter, {:message, %{msg | text: "alfred help"}}
+    assert_receive {:message, %{text: text}}
+    assert String.contains?(text, "Displays all help commands that match <query>")
+  end
+
+  @tag start_robot: true, name: "alfred"
+  test "help <query> - displays the usage for responders that match query", %{adapter: adapter, msg: msg} do
+    send adapter, {:message, %{msg | text: "alfred help test"}}
+    assert_receive {:message, %{text: text}}
+    assert text == "testuser: (this is a test) - did someone say test?"
+  end
+end
