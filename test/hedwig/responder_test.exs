2a55479c23d60d73b6787a8e2411ef73cb944b01
Sonny Scroggin <scrogson@gmail.com>
2015-12-14 21:13:46 -0600

    use the robot case

--- test/hedwig/responder_test.exs
+++ test/hedwig/responder_test.exs
@@ -1,39 +1,7 @@
 defmodule Hedwig.ResponderTest do
-  use ExUnit.Case, async: true
+  use Hedwig.RobotCase
 
   alias Hedwig.Responder
-
-  defmodule TestResponder do
-    use Hedwig.Responder
-
-    hear ~r/this is a test/i, msg do
-      send msg, "did someone say test?"
-    end
-
-    hear ~r/i love (\w+)/i, msg do
-      reply msg, "then why don't you marry #{msg.matches[1]}!?"
-    end
-
-    hear ~r/i like (?<subject>\w+)/i, msg do
-      emote msg, "likes #{msg.matches["subject"]} too!"
-    end
-
-    respond ~r/do you hear me\?/i, msg do
-      reply msg, "loud and clear!"
-    end
-  end
-
-  setup do
-    config = [name: "alfred", aka: "/", responders: [{TestResponder, []}]]
-    Application.put_env(:hedwig, Hedwig.TestRobot, config)
-    {:ok, robot} = Hedwig.start_robot(Hedwig.TestRobot)
-
-    current = self
-    adapter = :sys.get_state(robot).adapter
-    :sys.replace_state(adapter, fn state -> %{state | conn: current} end)
-
-    {:ok, adapter: adapter, robot: robot}
-  end
 
   test "respond_pattern" do
     robot = %Hedwig.Robot{name: "alfred", aka: nil}
