22b286c3b69d9cf397869107a11dc950bcbd956d
Johan Wärlander <johan@snowflake.nu>
2016-02-23 21:03:54 +0100

    Add tests for the robot inventory functions

--- test/hedwig_test.exs
+++ test/hedwig_test.exs
@@ -1,3 +1,14 @@
 defmodule HedwigTest do
-  use ExUnit.Case
+  use Hedwig.RobotCase
+
+  @tag start_robot: true
+  test "list started robots", %{robot: pid} do
+    assert [{_id, ^pid, _type, [Hedwig.Robot]}] = Hedwig.which_robots()
+  end
+
+  @tag start_robot: true, name: "codsworth"
+  test "find a robot by name", %{robot: pid} do
+    assert Hedwig.whereis("hedwig") == :undefined
+    assert Hedwig.whereis("codsworth") == pid
+  end
 end
