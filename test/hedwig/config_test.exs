4370cdc86d122e12b6ec2862d8cb42211300459e
Johan Wärlander <johan@snowflake.nu>
2016-02-23 17:38:18 +0100

    Test configuration parsing for ok + error cases

--- test/hedwig/config_test.exs
+++ test/hedwig/config_test.exs
@@ -6,4 +6,24 @@ defmodule Hedwig.ConfigTest do
       Hedwig.Robot.Supervisor.config(NoSuchApp.Robot, :no_such_app, [])
     end
   end
+
+  test "parse config (ok)" do
+    opts = [otp_app: :alfred, adapter: Hedwig.Adapters.Console]
+    result = Hedwig.Robot.Supervisor.parse_config(Alfred.Robot, opts)
+    assert result == {:alfred, Hedwig.Adapters.Console, []}
+  end
+
+  test "parse config (missing adapter keyword)" do
+    opts = [otp_app: :alfred]
+    assert_raise ArgumentError, fn ->
+      Hedwig.Robot.Supervisor.parse_config(Alfred.Robot, opts)
+    end
+  end
+
+  test "parse config (missing adapter code)" do
+    opts = [otp_app: :alfred, adapter: Hedwig.Adapters.NoSuchAdapter]
+    assert_raise ArgumentError, fn ->
+      Hedwig.Robot.Supervisor.parse_config(Alfred.Robot, opts)
+    end
+  end
 end
