a8266945ad6484c84fad4a960511d61122705fd5
Sonny Scroggin <scrogson@gmail.com>
2017-08-17 23:20:31 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule Hedwig.Mixfile do
   end
 
   defp deps do
-    [{:excoveralls, "~> 0.5", only: :test},
-     {:ex_doc, "~> 0.14", only: :dev}]
+    [{:excoveralls, "~> 0.7", only: :test},
+     {:ex_doc, "~> 0.16", only: :dev}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
