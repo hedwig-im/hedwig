6d396da79762c7effdb8529429ea8297dce8016b
doug schlenker <dougschlenker@gmail.com>
2017-08-22 12:43:48 -0700

    Update excoveralls & ex_doc deps (#73)
    updating deps

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule Hedwig.Mixfile do
   end
 
   defp deps do
-    [{:excoveralls, "~> 0.5", only: :test},
-     {:ex_doc, "~> 0.14", only: :dev}]
+    [{:excoveralls, "~> 0.7.2", only: :test},
+     {:ex_doc, "~> 0.16.3", only: :dev}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
